96d6e25d71ed30a32d1709d391d037f63f8943af
Mike <mikelrap@gmail.com>
2022-05-18 13:17:55 +0300

    types file

--- app/src/queries.ts
+++ app/src/queries.ts
@@ -1,9 +1,19 @@
 import { Web3ApiClient } from "@web3api/client-js";
+import { BlockChangeResult, BlockQuery, NodeStatusResult } from "./types";
 
 const client = new Web3ApiClient();
 const apiUri = "ens/near-api.eth";
 
-const gasPrice = async (blockId: string) => {
+export const status = async () => {
+  return await client.query<{ status: NodeStatusResult }>({
+    uri: apiUri,
+    query: `query {
+    status
+  }`,
+  });
+};
+
+export const gasPrice = async (blockId: string) => {
   return await client.query<{ gasPrice: BigInt }>({
     uri: apiUri,
     query: `query {
@@ -13,6 +23,20 @@ const gasPrice = async (blockId: string) => {
     }`,
     variables: {
       blockId: blockId,
+    },
+  });
+};
+
+export const blockChanges = async (blockQuery: BlockQuery) => {
+  return await client.query<{ blockChanges: BlockChangeResult }>({
+    uri: apiUri,
+    query: `query {
+    blockChanges(
+      blockQuery: $blockQuery
+    )
+  }`,
+    variables: {
+      blockQuery: blockQuery,
     },
   });
 };
