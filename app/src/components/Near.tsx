4ea3599cf8abdb13b034d1d96479784e7153ff9c
Brioline <33597141+Brioline@users.noreply.github.com>
2022-05-19 15:04:10 +0300

    refacroring (#4)
    Co-authored-by: yuliia gorgodze <taragorgodze@gmail.com>

--- app/src/components/Near.tsx
+++ app/src/components/Near.tsx
@@ -3,6 +3,40 @@ import { useQuery } from "../utils/hooks";
 import { Near_Queries } from "../utils/queries";
 
 export default function Near() {
+  //wallet 
+  const [, requestSignIn] = useQuery<{
+    requestSignIn: any;
+  }>(Near_Queries.requestSignIn);
+
+  const [, signOut] = useQuery<{
+    signOut: any;
+  }>(Near_Queries.signOut);
+
+  const [, isSignedIn] = useQuery<{
+    isSignedIn: any;
+  }>(Near_Queries.isSignedIn);
+
+  const [, getAccountId] = useQuery<{
+    getAccountId: any;
+  }>(Near_Queries.getAccountId);
+
+  // Transaction
+  const [, signAndSendTransaction] = useQuery<{
+    signAndSendTransaction: any ;
+  }>(Near_Queries.signAndSendTransaction);
+  const [, createsTransactionWithoutWallet] = useQuery<{
+    createsTransactionWithoutWallet: any;
+  }>(Near_Queries.createsTransactionWithoutWallet);
+
+  const [, signsTransactionWithoutWallet] = useQuery<{
+    signsTransactionWithoutWallet: any ;
+  }>(Near_Queries.signsTransactionWithoutWallet);
+
+  const [, signAndSendTransactionAsync] = useQuery<{
+    signAndSendTransactionAsync: any ;
+  }>(Near_Queries.signAndSendTransactionAsync);
+
+  // Queries 
   const [formatInput, setFormatInput] = useState(
     "9999989999999998370878870000000"
   );
@@ -27,10 +61,113 @@ export default function Near() {
 
   const [, getAccountState] = useQuery<{
     getAccountState: any /* BlockResult */;
-  }>(Near_Queries.getAccountState);
-  const [formatted, setFormatted] = useState<string>("");
-  const [parsed, setParsed] = useState<string>("");
+  }>(Near_Queries.getStatus);
 
+  const [, getGasPrice] = useQuery<{
+    gasPrice: any /* BlockResult */;
+  }>(Near_Queries.getGasPrice);
+  
+  const [, getStatus] = useQuery<{
+    getStatus: any /* BlockResult */;
+  }>(Near_Queries.getStatus);
+
+  const [, getExperimental_protocolConfig] = useQuery<{
+    getExperimental_protocolConfig: any /* BlockResult */;
+  }>(Near_Queries.getExperimental_protocolConfig);
+  
+  const [, getChunk] = useQuery<{
+    getChunk: any /* BlockResult */;
+  }>(Near_Queries.getChunk);
+
+  const [, getContractCodeChanges] = useQuery<{
+    getContractCodeChanges: any /* BlockResult */;
+  }>(Near_Queries.getContractCodeChanges);
+
+  const [, getBlockChanges] = useQuery<{
+    getBlockChanges: any /* BlockResult */;
+  }>(Near_Queries.getBlockChanges);
+
+  const [, getLightClientProof] = useQuery<{
+    getLightClientProof: any /* BlockResult */;
+  }>(Near_Queries.getLightClientProof);
+
+  const [, getAccessKeyChanges] = useQuery<{
+    getAccessKeyChanges: any /* BlockResult */;
+  }>(Near_Queries.getAccessKeyChanges);
+
+  const [, getAccountChanges] = useQuery<{
+    getAccountChanges: any /* BlockResult */;
+  }>(Near_Queries.getAccountChanges);
+  
+  const [, getContractStateChanges] = useQuery<{
+    getContractStateChanges: any /* BlockResult */;
+  }>(Near_Queries.getContractStateChanges);
+  
+  const [, getSingleAccessKeyChanges] = useQuery<{
+    getSingleAccessKeyChanges: any /* BlockResult */;
+  }>(Near_Queries.getSingleAccessKeyChanges);
+  
+  const [, getTxStatus] = useQuery<{
+    getTxStatus: any /* BlockResult */;
+  }>(Near_Queries.getTxStatus);
+  
+  const [formatted, setFormatted] = useState<string>("");
+
+  const [parsed, setParsed] = useState<string>("");
+  // wallet
+  const onRequestSignIn = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await requestSignIn();
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+
+  const onSignOut = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await signOut();
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+
+  const onIsSignedIn = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await isSignedIn();
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+  
+  const onGetAccountId = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await getAccountId();
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+  //transaction
+  const onSignAndSendTransaction = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await signAndSendTransaction();
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+  const onCreatesTransactionWithoutWallet = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await createsTransactionWithoutWallet();
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+  const onSignsTransactionWithoutWallet = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await signsTransactionWithoutWallet();
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+  const onSignAndSendTransactionAsync = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await signAndSendTransactionAsync();
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+  //QUERIES
   const onFormat = async (e: any) => {
     e.preventDefault();
     const { data, errors } = await format();
@@ -38,6 +175,84 @@ export default function Near() {
       console.log(data.formatNearAmount);
       setFormatted(data.formatNearAmount);
     }
+    errors && console.log(errors);
+  };
+
+  const onGetTxStatus = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await getTxStatus();
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+
+  const onGetSingleAccessKeyChanges = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await getSingleAccessKeyChanges();
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+
+  const onGetContractStateChanges = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await getContractStateChanges();
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+
+  const onGetContractCodeChanges = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await getContractCodeChanges();
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+  
+  const onGetAccountChanges = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await getAccountChanges();
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+
+  const onGetAccessKeyChanges = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await getAccessKeyChanges();
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+
+  const onGetLightClientProof = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await getLightClientProof();
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+
+  const onGetExperimental_protocolConfig = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await getExperimental_protocolConfig({ finality: "final" }); 
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+
+  const onGetChunk = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await getChunk();
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+
+  const onGetGasPrice = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await getGasPrice(); 
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+
+
+  const onGetBlockChanges = async (e: any) => {
+    e.preventDefault();
+    const { data, errors } = await getBlockChanges({ blockQuery: { block_id: "12", finality: null, syncCheckpoint: null } });
+    data && console.log(data);
     errors && console.log(errors);
   };
 
@@ -55,6 +270,13 @@ export default function Near() {
   const onGetBlock = async (e: any) => {
     e.preventDefault();
     const { data, errors } = await getBlock({ blockQuery: { block_id: "12" } });
+    data && console.log(data);
+    errors && console.log(errors);
+  };
+
+  const onGetStatus = async (e: any) => {
+    e.preventDefault();
+    const { data, errors} = await getStatus();
     data && console.log(data);
     errors && console.log(errors);
   };
@@ -96,7 +318,15 @@ export default function Near() {
           </span>
         )}
       </div>
-      <button onClick={onGetAccountState}>getAccountState</button>
+      <div style={{display: 'flex', flexDirection: 'column', width: '50%'}}>
+          <button onClick={onGetAccountState}>getAccountState</button>
+          <button onClick={onGetStatus}>getStatus</button>
+          <button onClick={onGetGasPrice}>getGasPrice</button>
+          <button onClick={onGetExperimental_protocolConfig}>getExperimental_protocolConfig</button>
+          <button onClick={onGetChunk}>getChunk</button>
+      </div>
+      {/* <button onClick={onGetBlock}>getBlock</button> */}
+      {/* <button onClick={onGetBlockChanges}>getBlockChanges</button> */}
     </div>
   );
 }
