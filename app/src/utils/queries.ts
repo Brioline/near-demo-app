a54b31b1a2591c588759a879b7ce1c9addfa2378
yuliia gorgodze <taragorgodze@gmail.com>
2022-05-19 13:59:20 +0200

    refacroring

--- app/src/utils/queries.ts
+++ app/src/utils/queries.ts
@@ -1,21 +1,28 @@
-import {
-  BlockChangeResult,
-  BlockQuery,
-  NodeStatusResult,
-  NearProtocolConfig,
-  Finality,
-  ChunkResult,
-  BlockResult,
-  LightClientProof,
-  ChangeResult,
-  FinalExecutionOutcome,
-} from "./types";
-
 export enum Near_Queries {
   formatNearAmount,
   parseNearAmount,
   getBlock,
   getAccountState,
+  getStatus,
+  getGasPrice,
+  getBlockChanges,
+  getExperimental_protocolConfig,
+  getChunk,
+  getLightClientProof,
+  getAccessKeyChanges,
+  getAccountChanges,
+  getContractCodeChanges,
+  getContractStateChanges,
+  getSingleAccessKeyChanges,
+  getTxStatus,
+  createsTransactionWithoutWallet,
+  signsTransactionWithoutWallet,
+  signAndSendTransaction,
+  signAndSendTransactionAsync,
+  requestSignIn,
+  signOut,
+  isSignedIn,
+  getAccountId
 }
 
 export const queries: Record<string, string> = {
@@ -38,215 +45,111 @@ export const queries: Record<string, string> = {
     getAccountState(
       accountId: $accountId
     )
-  }`,
-};
-
-export const status = async () => {
-  return await client.query<{ status: NodeStatusResult }>({
-    uri: apiUri,
-    query: `query {
+  }`, 
+  [Near_Queries.getStatus]: `query {
     status
   }`,
-  });
-};
-
-export const gasPrice = async (blockId: string) => {
-  return await client.query<{ gasPrice: BigInt }>({
-    uri: apiUri,
-    query: `query {
-      gasPrice(
-        blockId: $blockId
-      )
-    }`,
-    variables: {
-      blockId: blockId,
-    },
-  });
-};
-
-export const blockChanges = async (blockQuery: BlockQuery) => {
-  return await client.query<{ blockChanges: BlockChangeResult }>({
-    uri: apiUri,
-    query: `query {
+  [Near_Queries.getGasPrice]: `query {
+    gasPrice(
+      blockId: $blockId
+    )
+  }`,
+  [Near_Queries.getBlockChanges]: `query {
     blockChanges(
       blockQuery: $blockQuery
     )
   }`,
-    variables: {
-      blockQuery: blockQuery,
-    },
-  });
-};
-
-export const experimental_protocolConfig = async () => {
-  const blockReference = { finality: "final" as Finality };
-  await client.query<{ experimental_protocolConfig: NearProtocolConfig }>({
-    uri: apiUri,
-    query: `query {
-      experimental_protocolConfig(
-        blockReference: $blockReference
-      )
-    }`,
-    variables: {
-      blockReference: blockReference,
-    },
-  });
-};
-
-export const chunk = async (hash: BlockResult) => {
-  await client.query<{ chunk: ChunkResult }>({
-    uri: apiUri,
-    query: `query {
-      chunk(
-        chunkId:$chunkId
-      )
-    }`,
-    variables: {
-      chunkId: hash,
-    },
-  });
-};
-
-export const lightClientProof = async (
-  wrapper_lightClientProofRequest: LightClientProof
-) => {
-  await client.query<{ lightClientProof: LightClientProof }>({
-    uri: apiUri,
-    query: `query {
-      lightClientProof(
-        request: $request
-      )
-    }`,
-    variables: {
-      request: wrapper_lightClientProofRequest,
-    },
-  });
-};
-
-export const accessKeyChanges = async (
-  accountIdArray: string,
-  blockQuery: BlockQuery
-) => {
-  await client.query<{ accessKeyChanges: ChangeResult }>({
-    uri: apiUri,
-    query: `query {
-      accessKeyChanges(
-        accountIdArray: $accountIdArray
-        blockQuery: $blockQuery
-      )
-    }`,
-    variables: {
-      accountIdArray: accountIdArray,
-      blockQuery: blockQuery,
-    },
-  });
-};
-
-export const accountChanges = async (
-  accountIdArray: [nearApi.Account.accountId],
-  blockQuery: nearApi.Account
-) => {
-  await client.query<{ accountChanges: ChangeResult }>({
-    uri: apiUri,
-    query: `query {
-      accountChanges(
-        accountIdArray: $accountIdArray
-        blockQuery: $blockQuery
-      )
-    }`,
-    variables: {
-      accountIdArray: accountIdArray,
-      blockQuery: blockQuery,
-    },
-  });
-};
-
-export const contractCodeChanges = async (
-  accountIdArray: [nearApi.Account.accountId],
-  blockQuery: nearApi.Account
-) => {
-  await client.query<{ contractCodeChanges: ChangeResult }>({
-    uri: apiUri,
-    query: `query {
-      contractCodeChanges(
-        accountIdArray: $accountIdArray
-        blockQuery: $blockQuery
-      )
-    }`,
-    variables: {
-      accountIdArray: accountIdArray,
-      blockQuery: blockQuery,
-    },
-  });
-};
-
-export const contractStateChanges = async (
-  accountIdArray: [nearApi.Account.accountId],
-  blockQuery: nearApi.Account,
-  keyPrefix: string
-) => {
-  await client.query<{ contractStateChanges: ChangeResult }>({
-    uri: apiUri,
-    query: `query {
-      contractStateChanges(
-        accountIdArray: $accountIdArray
-        blockQuery: $blockQuery
-        keyPrefix: $keyPrefix
-      )
-    }`,
-    variables: {
-      accountIdArray: accountIdArray,
-      blockQuery: blockQuery,
-      keyPrefix: keyPrefix,
-    },
-  });
-};
-
-export const singleAccessKeyChanges = async (
-  blockQuery: nearApi.Account,
-  networkId: string
-) => {
-  const accessKeyArray = [
-    {
-      account_id: nearApi.Account.accountId,
-      public_key: (
-        await nearApi.Near.connection.signer.getPublicKey(
-          nearApi.Account.accountId,
-          networkId
-        )
-      ).toString(),
-    },
-  ];
-  await client.query<{ singleAccessKeyChanges: ChangeResult }>({
-    uri: apiUri,
-    query: `query {
-      singleAccessKeyChanges(
-        accessKeyArray: $accessKeyArray
-        blockQuery: $blockQuery
-      )
-    }`,
-    variables: {
-      accessKeyArray: accessKeyArray,
-      blockQuery: blockQuery,
-    },
-  });
-};
-
-export const txStatus = async (
-  accountId: nearApi.Account,
-  txHash: nearApi.providers.FinalExecutionOutcome
-) => {
-  await client.query<{ txStatus: FinalExecutionOutcome }>({
-    uri: apiUri,
-    query: `query {
-      txStatus(
-        txHash: $txHash
-        accountId: $accountId
-      )
-    }`,
-    variables: {
-      txHash,
-      accountId,
-    },
-  });
+  [Near_Queries.getExperimental_protocolConfig]: `query {
+    experimental_protocolConfig(
+      blockReference: $blockReference
+    )
+  }`,
+  [Near_Queries.getChunk]: `query {
+    chunk(
+      chunkId:$chunkId
+    )
+  }`,
+  [Near_Queries.getLightClientProof]: `query {
+    lightClientProof(
+      request: $request
+    )
+  }`,
+  [Near_Queries.getAccessKeyChanges]: `query {
+    accessKeyChanges(
+      accountIdArray: $accountIdArray
+      blockQuery: $blockQuery
+    )
+  }`,
+  [Near_Queries.getAccountChanges]: `query {
+    accountChanges(
+      accountIdArray: $accountIdArray
+      blockQuery: $blockQuery
+    )
+  }`,
+  [Near_Queries.getContractStateChanges]: `query {
+    contractStateChanges(
+      accountIdArray: $accountIdArray
+      blockQuery: $blockQuery
+      keyPrefix: $keyPrefix
+    )
+  }`,
+  [Near_Queries.getSingleAccessKeyChanges]: `query {
+    singleAccessKeyChanges(
+      accessKeyArray: $accessKeyArray
+      blockQuery: $blockQuery
+    )
+  }`,
+  [Near_Queries.getTxStatus]: `query {
+    txStatus(
+      txHash: $txHash
+      accountId: $accountId
+    )
+  }`,
+  // transaction
+  [Near_Queries.createsTransactionWithoutWallet]: `query {
+    createTransaction(
+      receiverId: $receiverId
+      actions: $actions
+      signerId: $signerId
+    )
+  }`,
+  [Near_Queries.signsTransactionWithoutWallet]: `query {
+    createTransaction(
+       receiverId: $receiverId
+       actions: $actions
+       signerId: $signerId
+   )
+  }`,
+  [Near_Queries.signAndSendTransaction]: `mutation {
+    signAndSendTransaction(
+     receiverId: $receiverId
+     actions: $actions
+     signerId: $signerId
+    )
+  }`,
+  [Near_Queries.signAndSendTransactionAsync]: `mutation {
+    signAndSendTransactionAsync(
+      receiverId: $receiverId
+      actions: $actions
+      signerId: $signerId
+    )
+  }`,
+  // wallet
+  [Near_Queries.requestSignIn]: `query {
+    requestSignIn(
+      contractId: $contractId,
+      methodNames: $methodNames,
+      successUrl: $successUrl,
+      failureUrl: $failureUrl,
+    )
+  }`,
+  [Near_Queries.signOut]: `query {
+    signOut
+  }`,
+  [Near_Queries.isSignedIn]: `query {
+    isSignedIn
+  }`,
+  [Near_Queries.getAccountId]: `query {
+    getAccountId
+  }`,
 };
